bb16cb526e3199f670f99fe662ba52a0cd325d37
Matt Kehrls <matthew.kehrls@gmail.com>
2018-05-02 16:33:01 -0400

    Add POST /todos route and refactored mongoose

---
+++ server/server.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyparser.json());
+
+app.post('/todos', (req, res) => {
+    // console.log(req.body);
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+})
+
+
+
+// var otherTodo = new Todo({
+//     text: 'Feed the cat',
+//     completed: true,
+//     completedAt: 123
+// });
+
+// otherTodo.save().then((doc) => {
+//     console.log("Saved Todo", doc);
+// },(err) => {
+//     console.log("There has been an error");
+// });
+
+// var newTodo = new Todo({
+//     text: 'Cook Dinner'
+// });
+
+// newTodo.save().then((doc) => {
+//     console.log("Saved Todo", doc);
+// },(err) => {
+//     console.log("There has been an error");
+// });
+
+// setTimeout(() => {db.close();}, 500);
