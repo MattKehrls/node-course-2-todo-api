a2e2d3d44cb702d3a7ed232c47014952395b20ef
Matt Kehrls <matthew.kehrls@gmail.com>
2018-05-15 19:05:32 -0400

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,9 +1,26 @@
 const {SHA256} = require("crypto-js");
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc';
 
-var token = jwt.sign(data, '123abc'); 
-var decoded = jwt.verify(token, '123abc');
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
+
+var hashedPassword = '$2a$10$4Z7vIwkXLWJJgzs0BRLFku.dfOPLmL42G4ZLvLzphROUrLIxoCSTW';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, '123abc'); 
+// var decoded = jwt.verify(token, '123abc');
